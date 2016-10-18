0d36781ae1384b137832b913b81138472c808bcf
maxim.syachin <MSyachin@luxoft.com>
2016-10-19 02:06:57 +0300

    val to var

--- src/main/kotlin/ru/finnetrolle/tele/rabbit/RabbitDto.kt
+++ src/main/kotlin/ru/finnetrolle/tele/rabbit/RabbitDto.kt
@@ -4,11 +4,11 @@ package ru.finnetrolle.tele.rabbit
  * Created by maxsyachin on 19.10.16.
  */
 data class ToSend(
-        val method: String,
-        val chatId: String,
-        val text: String,
-        val parseMode: String,
-        val disableWebPagePreview: Boolean,
-        val disableNotification: Boolean,
-        val replyToMessageId: Int
+        var method: String,
+        var chatId: String,
+        var text: String,
+        var parseMode: String,
+        var disableWebPagePreview: Boolean,
+        var disableNotification: Boolean,
+        var replyToMessageId: Int
 )
